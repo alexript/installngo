7e521f770d91d43342a4d0e3ee8d18055c28fb01
Alex 'Ript' Malyshev <alexript@gmail.com>
2024-03-29 16:40:18 +0300

    redice number of used lua libs

--- backends/lua/lua_libs.go
+++ backends/lua/lua_libs.go
@@ -23,10 +23,61 @@
 package lua
 
 import (
-	libs "github.com/vadv/gopher-lua-libs"
+	"github.com/vadv/gopher-lua-libs/argparse"
+	"github.com/vadv/gopher-lua-libs/base64"
+	"github.com/vadv/gopher-lua-libs/crypto"
+	"github.com/vadv/gopher-lua-libs/db"
+	"github.com/vadv/gopher-lua-libs/filepath"
+	"github.com/vadv/gopher-lua-libs/humanize"
+	"github.com/vadv/gopher-lua-libs/inspect"
+	"github.com/vadv/gopher-lua-libs/json"
+	"github.com/vadv/gopher-lua-libs/log"
+	plugin "github.com/vadv/gopher-lua-libs/plugin"
+	"github.com/vadv/gopher-lua-libs/regexp"
+	"github.com/vadv/gopher-lua-libs/storage"
+	"github.com/vadv/gopher-lua-libs/strings"
+	"github.com/vadv/gopher-lua-libs/template"
+	"github.com/vadv/gopher-lua-libs/time"
+	"github.com/vadv/gopher-lua-libs/xmlpath"
+	"github.com/vadv/gopher-lua-libs/yaml"
 	lua "github.com/yuin/gopher-lua"
 )
 
 func AttachLuaLibs(L *lua.LState) {
-	libs.Preload(L)
+	//	libs.Preload(L)
+	plugin.Preload(L)
+
+	argparse.Preload(L)
+	base64.Preload(L)
+	//	cert_util.Preload(L)
+	//	chef.Preload(L)
+	//	cloudwatch.Preload(L)
+	//	cmd.Preload(L)
+	crypto.Preload(L)
+	db.Preload(L)
+	filepath.Preload(L)
+	//	goos.Preload(L)
+	//	http.Preload(L)
+	humanize.Preload(L)
+	inspect.Preload(L)
+	//	ioutil.Preload(L)
+	json.Preload(L)
+	log.Preload(L)
+	//	pb.Preload(L)
+	//	pprof.Preload(L)
+	//	prometheus.Preload(L)
+	regexp.Preload(L)
+	///	runtime.Preload(L)
+	//	shellescape.Preload(L)
+	//	stats.Preload(L)
+	storage.Preload(L)
+	strings.Preload(L)
+	//	tac.Preload(L)
+	//	tcp.Preload(L)
+	//	telegram.Preload(L)
+	template.Preload(L)
+	time.Preload(L)
+	xmlpath.Preload(L)
+	yaml.Preload(L)
+	// zabbix.Preload(L)
 }
